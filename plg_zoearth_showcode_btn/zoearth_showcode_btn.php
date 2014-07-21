86348fdd325bb59752e5bafe35d194fd0a372b3b
zoearthmoon@gmail.com <zoearthmoon@gmail.com>
2014-07-21 22:56:23 +0800

    確立命名規則

--- plg_zoearth_showcode_btn/zoearth_showcode_btn.php
+++ plg_zoearth_showcode_btn/zoearth_showcode_btn.php
@@ -1,20 +1,20 @@
 <?php
 defined('_JEXEC') or die;
 
-class plgZoearthShowCodeBtn extends JPlugin
+class plgButtonZoearth_ShowCode_Btn extends JPlugin
 {
     public function onDisplay($name)
     {
 // 		$txt = $this->params->get('custom_text', '[set your custom text in plugin parameters]');
-// 		$label = $this->params->get('label', 'Custom Text');
-// 		$jsCode = "
-//                 function insertCustText(editor) {
-//                     jInsertEditorText('".$txt."', editor);
-//                 }
-// 				";
+ 		$label = $this->params->get('label', 'Custom Text');
+ 		$jsCode = "
+                 function insertCustText(editor) {
+                     jInsertEditorText('".$txt."', editor);
+                }
+ 				";
 				
-// 		$doc = JFactory::getDocument();
-// 		$doc->addScriptDeclaration($jsCode);
+ 		$doc = JFactory::getDocument();
+ 		$doc->addScriptDeclaration($jsCode);
 		
         $button = new JObject();				
 		$button->modal = false;
@@ -24,6 +24,9 @@ class plgZoearthShowCodeBtn extends JPlugin
 		//$button->onclick = 'insertCustText(\''.$name.'\');return false;';				$button->link = '#';
         $button->onclick = 'alert("good!");return false;';
         $button->link = '#';
+		
+		echo 'btn testing';
+		
         return $button;
     }
 }
