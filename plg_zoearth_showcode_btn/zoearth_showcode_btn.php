34dc29ecaed7667a7499f3c962933cef7c463788
zoearthmoon@gmail.com <zoearthmoon@gmail.com>
2014-07-23 11:45:22 +0800

    初步完成 後台編輯器按鈕功能

--- plg_zoearth_showcode_btn/zoearth_showcode_btn.php
+++ plg_zoearth_showcode_btn/zoearth_showcode_btn.php
@@ -5,28 +5,48 @@ class plgButtonZoearth_ShowCode_Btn extends JPlugin
 {
     public function onDisplay($name)
     {
-// 		$txt = $this->params->get('custom_text', '[set your custom text in plugin parameters]');
- 		$label = $this->params->get('label', 'Custom Text');
- 		$jsCode = "
-                 function insertCustText(editor) {
-                     jInsertEditorText('".$txt."', editor);
-                }
- 				";
-				
- 		$doc = JFactory::getDocument();
- 		$doc->addScriptDeclaration($jsCode);
-		
+		?>
+		<script language="Javascript">
+		var showCodeInput = function (){
+			jQuery('#showCodeInputModal').modal('show');
+		};
+		jQuery(document).ready(function() {
+			jQuery('#showCodeInputModal').on('show',function (){
+				//出現modal時清空textarea
+				jQuery("#showCodeContent").val("");
+			});
+			//儲存
+			jQuery("#zoearthCodeInput").click(function(){
+				var showCodeContent = jQuery("#showCodeContent").val().trim();
+				showCodeContent = '<pre class="prettyprint" style="background-color: #c0c0c0;">'+showCodeContent+'</pre><br>';
+				jInsertEditorText(showCodeContent, 'jform_articletext');
+				jQuery('#showCodeInputModal').modal('hide');
+			});
+			
+			
+		});
+		</script>
+		<div id="showCodeInputModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+				<h3 id="myModalLabel"><?php echo JText::_('PLG_ZOEARTH_SHOW_CODE_BTN')?></h3>
+			</div>
+			<div class="modal-body">
+				<textarea id="showCodeContent" rows="8" class="span12"></textarea>
+			</div>
+			<div class="modal-footer">
+				<a class="btn" data-dismiss="modal" aria-hidden="true"><?php echo JText::_('JOFF') ?></a>
+				<a class="btn btn-primary" id="zoearthCodeInput" ><?php echo JText::_('JSUBMIT') ?></a>
+			</div>
+		</div>
+		<?php
         $button = new JObject();				
-		$button->modal = false;
+		$button->modal = FALSE;
 		$button->class = 'btn';
-        $button->text = 'ZoearthShowCode';
-        $button->name = 'ZoearthShowCode name';
-		//$button->onclick = 'insertCustText(\''.$name.'\');return false;';				$button->link = '#';
-        $button->onclick = 'alert("good!");return false;';
+        $button->text = JText::_('PLG_ZOEARTH_SHOW_CODE_BTN');
+        $button->name = 'ZoearthShowCode Name';
+        $button->onclick = 'showCodeInput();return false;';
         $button->link = '#';
-		
-		echo 'btn testing';
-		
         return $button;
     }
 }
