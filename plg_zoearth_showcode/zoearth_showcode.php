7fb54f5ed6e7edbc68a259fd60567b8326c747ab
zoearthmoon@gmail.com <zoearthmoon@gmail.com>
2014-07-23 13:47:44 +0800

    修改 CSS

--- plg_zoearth_showcode/zoearth_showcode.php
+++ plg_zoearth_showcode/zoearth_showcode.php
@@ -24,7 +24,7 @@ class plgSystemZoearth_ShowCode extends JPlugin
 	overflow:auto;
 	background: #f0f0f0 url(data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAsAAASwCAYAAAAt7rCDAAAABHNCSVQICAgIfAhkiAAAAQJJREFUeJzt0kEKhDAMBdA4zFmbM+W0upqFOhXrDILwsimFR5pfMrXW5jhZr7PwRlxVX8//jNHrGhExjXzdu9c5IiIz+7iqVmB7Hwp4OMa2nhhwN/PRGEMBh3Zjt6KfpzPztxW9MSAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzB8HS+J9kUTvzEDMwAAAABJRU5ErkJggg==) left top repeat-y;
 	border: 1px solid #ccc;
-	padding: 10px 10px 10px 21px;
+	padding: 10px 10px 10px 21px !important;
 	max-height:1000px;
 	line-height: 1.2em;
 }
